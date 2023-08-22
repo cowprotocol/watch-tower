35b679d39be8a9ce7ec74658eba8e3753b4310a2
mfw78 <mfw78@protonmail.com>
2023-08-22 13:04:09 +0000

    test: add fail test

--- actions/utils.spec.ts
+++ actions/utils.spec.ts
@@ -1,9 +1,13 @@
 import * as composableCow from './artifacts/ComposableCoW.json'
+import * as extensibleFallbackHandler from './artifacts/ExtensibleFallbackHandler.json'
 import { checkInterface } from './utils'
 
-describe('test contract bytecode', () => {
+describe('test supports composable cow interface from bytecode', () => {
     it('should pass', async () => {
-        console.log(composableCow.deployedBytecode.object)
         expect(await checkInterface(composableCow.deployedBytecode.object)).toBe(true)
+    })
+
+    it('should fail', async () => {
+        expect(await checkInterface(extensibleFallbackHandler.deployedBytecode.object)).toBe(false)
     })
 })
