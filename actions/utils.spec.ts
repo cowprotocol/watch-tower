38c22b53ab7966bd6e9d221db14d5228227f769c
mfw78 <mfw78@protonmail.com>
2023-08-22 16:34:31 +0000

    test: coverage

--- actions/utils.spec.ts
+++ actions/utils.spec.ts
@@ -11,3 +11,22 @@ describe('test supports composable cow interface from bytecode', () => {
         expect(isCompatible(extensibleFallbackHandler.deployedBytecode.object)).toBe(false)
     })
 })
+
+describe('test against concrete examples', () => {
+    const signatures = ['0x1c7662c8', '0x26e0a196']
+
+    it('should pass with both selectors', () => {
+        expect(isCompatible('0x1c7662c826e0a196')).toBe(true)
+    })
+
+    // using `forEach` here, be careful not to do async tests.
+    signatures.forEach((s) => {
+        it(`should fail with only selector ${s}`, () => {
+            expect(isCompatible(s)).toBe(false)
+        })
+    })
+
+    it('should fail with no selectors', () => {
+        expect(isCompatible('0xdeadbeefdeadbeef')).toBe(false)
+    })
+})
