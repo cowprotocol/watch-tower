5dc46f385cc31847354a5088c019f7ae1d1d16d6
Anxo Rodriguez <anxolin@gmail.com>
2023-09-20 15:53:10 +0100

    Allow to process single TX

--- actions/test/run_local.ts
+++ actions/test/run_local.ts
@@ -25,9 +25,24 @@ const main = async () => {
   // Get provider
   const provider = await getProvider(testRuntime.context, chainId);
 
-  // Run one of the 2 Execution modes (single block, or watch mode)
+  // Run one of the 3 Execution modes:
+  //  - Single tx: Process just one tx
+  //  - Single block: Process just one block
+  //  - Watch mode: Watch for new blocks, and process them
+  const txEnv = process.env.TX;
   const blockNumberEnv = process.env.BLOCK_NUMBER;
-  if (blockNumberEnv) {
+  if (txEnv) {
+    // Execute once, for a specific tx
+    console.log(
+      `[run_local] Processing ONCE for a specific transaction: ${txEnv}...`
+    );
+
+    await processTx(provider, txEnv, chainId, testRuntime).catch(() => {
+      exit(101);
+    });
+
+    console.log(`[run_local] Transaction ${txEnv} has been processed.`);
+  } else if (blockNumberEnv) {
     // Execute once, for a specific block
     const isLatest = blockNumberEnv === "latest";
     const blockNumber = isLatest
@@ -72,56 +87,116 @@ async function processBlock(
   );
   let hasErrors = false;
   for (const transaction of blockWithTransactions.transactions) {
-    const receipt = await provider.getTransactionReceipt(transaction.hash);
-    if (receipt) {
-      const {
-        hash,
-        from,
-        value,
-        nonce,
-        gasLimit,
-        maxPriorityFeePerGas,
-        maxFeePerGas,
-      } = transaction;
-
-      const testTransactionEvent: TestTransactionEvent = {
-        blockHash: block.hash,
-        blockNumber: block.number,
-        from,
-        hash,
-        network: chainId.toString(),
-        logs: receipt.logs,
-        input: "",
-        value: value.toString(),
-        nonce: nonce.toString(),
-        gas: gasLimit.toString(),
-        gasUsed: receipt.gasUsed.toString(),
-        cumulativeGasUsed: receipt.cumulativeGasUsed.toString(),
-        gasPrice: receipt.effectiveGasPrice.toString(),
-        gasTipCap: maxPriorityFeePerGas ? maxPriorityFeePerGas.toString() : "",
-        gasFeeCap: maxFeePerGas ? maxFeePerGas.toString() : "",
-        transactionHash: transaction.hash,
-      };
-
-      // run action
-      const result = await testRuntime
-        .execute(addContract, testTransactionEvent)
-        .then(() => true)
-        .catch((e) => {
-          hasErrors = true;
-          console.error(
-            `[run_local] Error running "addContract" action for TX:`,
-            e
-          );
-          return false;
-        });
-      console.log(
-        `[run_local] Result of "addContract" action for TX ${hash}: ${_formatResult(
-          result
-        )}`
-      );
-    }
+    hasErrors ||= await !_processTx(
+      provider,
+      block,
+      chainId,
+      testRuntime,
+      transaction
+    );
   }
+
+  hasErrors ||= !(await _pollAndPost({ block, chainId, testRuntime }));
+
+  if (hasErrors) {
+    throw new Error("[run_local] Errors found in processing block");
+  }
+}
+
+async function processTx(
+  provider: ethers.providers.Provider,
+  tx: string,
+  chainId: number,
+  testRuntime: TestRuntime
+) {
+  // Execute once, for a specific tx
+  console.log(
+    `[run_local] Processing ONCE for a specific transaction: ${tx}...`
+  );
+  const transaction = await provider.getTransaction(tx);
+  if (!transaction.blockNumber) {
+    throw new Error(`The transaction ${tx} is not mined yet (no blockNumber)`);
+  }
+  const block = await provider.getBlock(transaction.blockNumber);
+  if (!transaction) {
+    throw new Error(`[run_local] Transaction ${tx} not found`);
+  }
+
+  await _processTx(provider, block, chainId, testRuntime, transaction);
+  await _pollAndPost({ block, chainId, testRuntime });
+}
+
+async function _processTx(
+  provider: ethers.providers.Provider,
+  block: ethers.providers.Block,
+  chainId: number,
+  testRuntime: TestRuntime,
+  transaction: ethers.providers.TransactionResponse
+): Promise<boolean> {
+  const receipt = await provider.getTransactionReceipt(transaction.hash);
+  if (receipt) {
+    const {
+      hash,
+      from,
+      value,
+      nonce,
+      gasLimit,
+      maxPriorityFeePerGas,
+      maxFeePerGas,
+    } = transaction;
+
+    const testTransactionEvent: TestTransactionEvent = {
+      blockHash: block.hash,
+      blockNumber: block.number,
+      from,
+      hash,
+      network: chainId.toString(),
+      logs: receipt.logs,
+      input: "",
+      value: value.toString(),
+      nonce: nonce.toString(),
+      gas: gasLimit.toString(),
+      gasUsed: receipt.gasUsed.toString(),
+      cumulativeGasUsed: receipt.cumulativeGasUsed.toString(),
+      gasPrice: receipt.effectiveGasPrice.toString(),
+      gasTipCap: maxPriorityFeePerGas ? maxPriorityFeePerGas.toString() : "",
+      gasFeeCap: maxFeePerGas ? maxFeePerGas.toString() : "",
+      transactionHash: transaction.hash,
+    };
+
+    // run action
+    const result = await testRuntime
+      .execute(addContract, testTransactionEvent)
+      .then(() => true)
+      .catch((e) => {
+        console.error(
+          `[run_local] Error running "addContract" action for TX:`,
+          e
+        );
+        return false;
+      });
+    console.log(
+      `[run_local] Result of "addContract" action for TX ${hash}: ${_formatResult(
+        result
+      )}`
+    );
+
+    return result;
+  }
+
+  return true;
+}
+
+async function _pollAndPost({
+  block,
+  chainId,
+  testRuntime,
+}: {
+  block: ethers.providers.Block;
+  chainId: number;
+  testRuntime: TestRuntime;
+}) {
+  const blockNumber = block.number;
 
   // Block watcher for creating new orders
   const testBlockEvent = new TestBlockEvent();
@@ -135,8 +210,7 @@ async function processBlock(
   const result = await testRuntime
     .execute(checkForAndPlaceOrder, testBlockEvent)
     .then(() => true)
-    .catch((e) => {
-      hasErrors = true;
+    .catch(() => {
       console.log(`[run_local] Error running "checkForAndPlaceOrder" action`);
       return false;
     });
@@ -146,9 +220,7 @@ async function processBlock(
     )}`
   );
 
-  if (hasErrors) {
-    throw new Error("[run_local] Errors found in processing block");
-  }
+  return result;
 }
 
 async function _getRunTime(chainId: SupportedChainId): Promise<TestRuntime> {
