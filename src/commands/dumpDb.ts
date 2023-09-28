adcd0b7d0f86d0996b4ea8165ca947ba67919e34
mfw78 <mfw78@protonmail.com>
2023-09-28 06:39:29 +0000

    feat: dump db

---
+++ src/commands/dumpDb.ts
@@ -0,0 +1,17 @@
+import { DumpDbOptions, Registry } from "../types";
+import { DBService } from "../utils";
+
+/**
+ * Dump the database as JSON to STDOUT for a given chain ID
+ * @param options A dict, but essentially just the chainId
+ */
+export async function dumpDb(options: DumpDbOptions) {
+  const { chainId } = options;
+
+  Registry.dump(DBService.getInstance(), chainId.toString())
+    .then((dump) => console.log(dump))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,3 +1,4 @@
 export * from "./run";
 export * from "./replayBlock";
 export * from "./replayTx";
+export * from "./dumpDb";
