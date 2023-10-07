7117753f2f4d25eb03f690c34431da9b2685b11a
mfw78 <mfw78@protonmail.com>
2023-10-07 03:42:15 +0000

    fix: metric exports

--- src/utils/metrics.ts
+++ src/utils/metrics.ts
@@ -1,151 +1,127 @@
 import client from "prom-client";
 
-const blockHeight = new client.Gauge({
+export const blockHeight = new client.Gauge({
   name: "watch_tower_block_height",
   help: "Block height of the block watcher",
   labelNames: ["chain_id"],
 });
 
-const reorgDepth = new client.Gauge({
+export const reorgDepth = new client.Gauge({
   name: "watch_tower_reorg_depth",
   help: "Depth of the current reorg",
   labelNames: ["chain_id"],
 });
 
-const blockTime = new client.Gauge({
+export const blockTime = new client.Gauge({
   name: "watch_tower_block_time_seconds",
   help: "Time since last block",
   labelNames: ["chain_id"],
 });
 
-const eventsProcessedTotal = new client.Counter({
+export const eventsProcessedTotal = new client.Counter({
   name: "watch_tower_events_processed_total",
   help: "Total number of events processed",
   labelNames: ["chain_id"],
 });
 
-const reorgsTotal = new client.Counter({
+export const reorgsTotal = new client.Counter({
   name: "watch_tower_reorg_total",
   help: "Total number of reorgs",
   labelNames: ["chain_id"],
 });
 
-const newContractsTotal = new client.Counter({
+export const newContractsTotal = new client.Counter({
   name: "watch_tower_new_contracts_total",
   help: "Total number of new contracts",
   labelNames: ["chain_id"],
 });
 
-const singleOrdersTotal = new client.Counter({
+export const singleOrdersTotal = new client.Counter({
   name: "watch_tower_single_orders_total",
   help: "Total number of single orders processed",
   labelNames: ["chain_id"],
 });
 
-const merkleRootTotal = new client.Counter({
+export const merkleRootTotal = new client.Counter({
   name: "watch_tower_merkle_roots_total",
   help: "Total number of merkle roots processed",
   labelNames: ["chain_id"],
 });
 
-const addContractsErrorsTotal = new client.Counter({
+export const addContractsErrorsTotal = new client.Counter({
   name: "watch_tower_add_contracts_errors_total",
   help: "Total number of add contracts errors",
   labelNames: ["chain_id", "function"],
 });
 
-const addContractsRunDurationSeconds = new client.Histogram({
+export const addContractsRunDurationSeconds = new client.Histogram({
   name: "watch_tower_add_contracts_run_duration_seconds",
   help: "Duration of add contracts run",
   labelNames: ["chain_id"],
 });
 
-const processBlockDurationSeconds = new client.Histogram({
+export const processBlockDurationSeconds = new client.Histogram({
   name: "watch_tower_process_block_duration_seconds",
   help: "Duration of process block",
   labelNames: ["chain_id"],
 });
 
-const totalActiveOwners = new client.Gauge({
+export const totalActiveOwners = new client.Gauge({
   name: "watch_tower_active_owners_total",
   help: "Total number of owners with active orders",
   labelNames: ["chain_id"],
 });
 
-const totalActiveOrders = new client.Gauge({
+export const totalActiveOrders = new client.Gauge({
   name: "watch_tower_active_orders_total",
   help: "Total number of active orders",
   labelNames: ["chain_id"],
 });
 
-const totalOrderBookDiscreteOrders = new client.Counter({
+export const totalOrderBookDiscreteOrders = new client.Counter({
   name: "watch_tower_orderbook_discrete_orders_total",
   help: "Total number of discrete orders posted to the orderbook",
   labelNames: ["chain_id", "handler", "owner", "id"],
 });
 
-const totalOrderBookErrors = new client.Counter({
+export const totalOrderBookErrors = new client.Counter({
   name: "watch_tower_orderbook_errors_total",
   help: "Total number of errors when interacting with the orderbook",
   labelNames: ["chain_id", "handler", "owner", "id", "status", "error"],
 });
 
-const totalPollingRuns = new client.Counter({
+export const totalPollingRuns = new client.Counter({
   name: "watch_tower_polling_runs_total",
   help: "Total number of polling runs",
   labelNames: ["chain_id", "handler", "owner", "id"],
 });
 
-const totalPollingOnChainChecks = new client.Counter({
+export const totalPollingOnChainChecks = new client.Counter({
   name: "watch_tower_polling_onchain_checks_total",
   help: "Total number of on-chain hint checks",
   labelNames: ["chain_id", "handler", "owner", "id"],
 });
 
-const pollingOnChainDurationSeconds = new client.Histogram({
+export const pollingOnChainDurationSeconds = new client.Histogram({
   name: "watch_tower_polling_onchain_duration_seconds",
   help: "Duration of on-chain hint checks",
   labelNames: ["chain_id", "handler", "owner", "id"],
 });
 
-const totalPollingOnChainInvalidInterfaces = new client.Counter({
+export const totalPollingOnChainInvalidInterfaces = new client.Counter({
   name: "watch_tower_polling_onchain_invalid_interface_total",
   help: "Total number of invalid on-chain hint interface",
   labelNames: ["chain_id", "handler", "owner", "id"],
 });
 
-const totalPollingOnChainEthersErrors = new client.Counter({
+export const totalPollingOnChainEthersErrors = new client.Counter({
   name: "watch_tower_polling_onchain_ethers_errors_total",
   help: "Total number of ethers on-chain hint errors",
   labelNames: ["chain_id", "handler", "owner", "id"],
 });
 
-const totalPollingUnexpectedErrors = new client.Counter({
+export const totalPollingUnexpectedErrors = new client.Counter({
   name: "watch_tower_polling_unexpected_errors_total",
   help: "Total number of unexpected polling errors",
   labelNames: ["chain_id", "handler", "owner", "id"],
 });
-
-export {
-  blockHeight,
-  reorgDepth,
-  blockTime,
-  eventsProcessedTotal,
-  reorgsTotal,
-  newContractsTotal,
-  singleOrdersTotal,
-  merkleRootTotal,
-  addContractsErrorsTotal,
-  addContractsRunDurationSeconds,
-  processBlockDurationSeconds,
-  totalActiveOwners,
-  totalActiveOrders,
-  totalOrderBookDiscreteOrders,
-  totalOrderBookErrors,
-  totalPollingRuns,
-  totalPollingOnChainChecks,
-  totalPollingOnChainInvalidInterfaces,
-  totalPollingOnChainEthersErrors,
-  pollingOnChainDurationSeconds,
-  totalPollingUnexpectedErrors,
-};
