534c6a9b97b6f7a3aa587d6ca3fde52247976204
Anxo Rodriguez <anxolin@gmail.com>
2023-11-24 17:46:14 +0000

    feat: add basic auth

--- src/types/index.ts
+++ src/types/index.ts
@@ -29,6 +29,8 @@ export type ChainConfigOptions = {
   watchdogTimeout: number;
   orderBookApi?: string;
   filterPolicyConfig?: string;
+  rpcUser?: string;
+  rpcPassword?: string;
   // filterPolicyConfigAuthToken?: string; // TODO: Implement authToken
 };
 
